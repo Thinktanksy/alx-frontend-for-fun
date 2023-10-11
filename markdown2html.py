02f4e63df27e6dcbe657758f6f38ec1f0cf0a22c
Joseph Williams <richlike.joseph@gmail.com>
2023-10-11 11:58:23 +0000

    For fun

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-This is a script to convert a Markdown file to HTML.
+This is a script to convert a Markdown file to HTML with support for various custom syntax.
 
 Usage:
     ./markdown2html.py [input_file] [output_file]
@@ -17,30 +17,40 @@ import argparse
 import re
 import sys
 import os
+import hashlib
 
 def convert_md_to_html(input_file, output_file):
     # Read the contents of the input file
     with open(input_file) as f:
-        md_content = f.readlines()
+        md_content = f.read()
 
-    html_content = []
-    for line in md_content:
-        # Check if the line is a heading
-        match = re.match(r'(#){1,6} (.*)', line)
-        if match:
-            h_level = len(match.group(1))
-            h_content = match.group(2)
-            html_content.append('<h{0}>{1}</h{0}>\n'.format(h_level, h_content))
-        else:
-            html_content.append(line)
+    # Define a regular expression pattern to match [[text]] for MD5 hashing
+    md5_pattern = r'\[\[(.*?)\]\]'
+    
+    # Use re.sub to replace [[text]] with its MD5 hash (lowercase)
+    def md5_replace(match):
+        content = match.group(1)
+        return hashlib.md5(content.encode()).hexdigest().lower()
+
+    html_content = re.sub(md5_pattern, md5_replace, md_content)
+
+    # Define a regular expression pattern to match ((text)) for case-insensitive content replacement
+    replace_pattern = r'\(\((.*?)\)\)'
+    
+    # Use re.sub to remove all 'c' characters (case-insensitive) from the content
+    def replace_content(match):
+        content = match.group(1)
+        return content.replace('c', '', flags=re.IGNORECASE)
+
+    html_content = re.sub(replace_pattern, replace_content, html_content)
 
     # Write the HTML content to the output file
     with open(output_file, 'w') as f:
-        f.writelines(html_content)
+        f.write(html_content)
 
 if __name__ == '__main__':
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML with support for various custom syntax')
     parser.add_argument('input_file', help='path to input markdown file')
     parser.add_argument('output_file', help='path to output HTML file')
 
@@ -56,6 +66,6 @@ if __name__ == '__main__':
         sys.stderr.write('Missing {0}\n'.format(args.input_file))
         sys.exit(1)
 
-    # Convert the markdown file to HTML
+    # Convert the markdown file to HTML with custom syntax handling
     convert_md_to_html(args.input_file, args.output_file)
 
