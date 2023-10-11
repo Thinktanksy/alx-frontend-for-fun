5cec7eca4e7907690d51d4e0f62b8d834ab7a862
Joseph Williams <richlike.joseph@gmail.com>
2023-10-11 11:43:57 +0000

    For fun

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
+"""
+This is a script to convert a Markdown file to HTML.
+
+Usage:
+    ./markdown2html.py [input_file] [output_file]
+
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html
+"""
 
 import argparse
 import pathlib
 import re
-import sys  # Add this line to import the sys module
+import sys
 
 def convert_md_to_html(input_file, output_file):
-    '''
-    Converts markdown file to HTML file
-    '''
     # Read the contents of the input file
     with open(input_file, encoding='utf-8') as f:
         md_content = f.readlines()
@@ -18,11 +28,8 @@ def convert_md_to_html(input_file, output_file):
         # Check if the line is a heading
         match = re.match(r'(#){1,6} (.*)', line)
         if match:
-            # Get the level of the heading
             h_level = len(match.group(1))
-            # Get the content of the heading
             h_content = match.group(2)
-            # Append the HTML equivalent of the heading
             html_content.append('<h{0}>{1}</h{0}>\n'.format(h_level, h_content))
         else:
             html_content.append(line)
@@ -31,12 +38,18 @@ def convert_md_to_html(input_file, output_file):
     with open(output_file, 'w', encoding='utf-8') as f:
         f.writelines(html_content)
 
-if __name__ == '__main__':
+if __name__ == '__main':
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Convert markdown to HTML')
     parser.add_argument('input_file', help='path to input markdown file')
     parser.add_argument('output_file', help='path to output HTML file')
+
     args = parser.parse_args()
+
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py [input_file] [output_file]", file=sys.stderr)
+        sys.exit(1)
 
     # Check if the input file exists
     input_path = pathlib.Path(args.input_file)
